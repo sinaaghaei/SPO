3989b2ef1d62ee20b4af9f05224f2434994961b1
Sina <saghaei@usc.edu>
2020-11-23 05:13:22 -0800

    calibration code

---
+++ solver/sp_calibrator.py
@@ -0,0 +1,139 @@
+'''
+In this module, we calibrate the hyper parameters _lambda and _alpha for a given instance.
+We create a sequence of lambdas. For each value in the sequence we train the model on the training set and evaluate
+spo_loss on the calibration set. We return the one with the lowest loss on the calibration set.
+
+For calibrating alpha, we first calibrate lambda and then given the best lambda, similarly we find the best alpha.
+'''
+
+from solver.sp_least_square import SpLeastSquare
+from solver.sp_reformulation import SpReformulation
+from solver.sp_evaluation import *
+
+from gurobipy import *
+import numpy as np
+
+
+class SpCalibrator:
+    def __init__(self, x_train, c_train, x_calibration, c_calibration, oracle, regularization, time_limit, num_lambda,
+                 lambda_max, lambda_min_ratio, num_alpha,
+                 alpha_max, alpha_min_ratio, loss_metric="SPO_loss", acyclic=False,
+                 regularize_first_column_B=False,
+                 upper_bound_B_present=False, upper_bound_B=10.0 ** 6):
+        self.x_train = x_train
+        self.c_train = c_train
+        self.x_calibration = x_calibration
+        self.c_calibration = c_calibration
+
+        self.oracle = oracle
+        self.regularization = regularization
+        self.time_limit = time_limit
+        self.acyclic = acyclic
+        self.regularize_first_column_B = regularize_first_column_B
+        self.upper_bound_B_present = upper_bound_B_present
+        self.upper_bound_B = upper_bound_B
+        self.num_lambda = num_lambda
+        self.lambda_max = lambda_max
+        self.lambda_min_ratio = lambda_min_ratio
+        self.num_alpha = num_alpha
+        self.alpha_max = alpha_max
+        self.alpha_min_ratio = alpha_min_ratio
+        self.loss_metric = loss_metric
+
+        # Constructing the lambda sequence
+        self.lambda_sequence = None
+        if self.num_lambda == 1 and self.lambda_max == 0:
+            lambda_sequence = [0.0]
+        else:
+            lambda_min = self.lambda_max * self.lambda_min_ratio
+            gap = np.log(self.lambda_max) - np.log(lambda_min)
+            log_lambdas = np.arange(np.log(lambda_min), np.log(self.lambda_max), gap / self.num_lambda)
+            self.lambda_sequence = np.exp(log_lambdas)
+
+        # Constructing the alpha sequence
+        self.alpha_sequence = self.lambda_sequence
+
+    def calibrate(self, method):
+        best_B = None
+        best_lambda = None
+        best_alpha = None
+        if method == "LSE":
+            best_B, best_lambda = self.calibrate_LSE()
+        elif method == "SPO_plus":
+            best_B, best_lambda = self.calibrate_SPO_plus()
+        elif method == "SPO_plus_LSE":
+            best_B, best_lambda, best_alpha = self.calibrate_SPO_plus_LSE()
+        else:
+            raise Exception("Wrong method name")
+
+        return best_B, best_lambda, best_alpha
+
+    def calibrate_LSE(self):
+        best_B = None
+        best_lambda = None
+        best_loss = np.PINF
+        for lambda_cur in self.lambda_sequence:
+            sp_least_square_instance = SpLeastSquare(self.x_train, self.c_train, self.oracle, self.regularization,
+                                                     lambda_cur, self.time_limit)
+            sol_dict = sp_least_square_instance.solve_model()
+            if sol_dict['status'] in ["infeasible", "inf_or_unbd"]:
+                pass
+            else:
+                B_new = sol_dict['B_ast']
+                if self.loss_metric == "SPO_loss":
+                    spo_loss_value = spo_loss(B_new, self.x_calibration, self.c_calibration, self.oracle)
+                    if spo_loss_value < best_loss:
+                        best_loss = spo_loss_value
+                        best_B = B_new
+                        best_lambda = lambda_cur
+
+        return best_B, best_lambda
+
+    def calibrate_SPO_plus(self):
+        best_B = None
+        best_lambda = None
+        best_loss = np.PINF
+        for lambda_cur in self.lambda_sequence:
+            sp_reformulation_instance = SpReformulation(self.x_train, self.c_train, self.oracle, self.regularization,
+                                                        lambda_cur, 0, self.time_limit)  # alpha = 0
+            sol_dict = sp_reformulation_instance.solve_model()
+            if sol_dict['status'] in ["infeasible", "inf_or_unbd"]:
+                pass
+            else:
+                B_new = sol_dict['B_ast']
+                if self.loss_metric == "SPO_loss":
+                    spo_loss_value = spo_loss(B_new, self.x_calibration, self.c_calibration, self.oracle)
+                    if spo_loss_value < best_loss:
+                        best_loss = spo_loss_value
+                        best_B = B_new
+                        best_lambda = lambda_cur
+
+        return best_B, best_lambda
+
+    def calibrate_SPO_plus_LSE(self):
+        '''
+        We first find the best lambda without including alpha regularizer in the objective by setting alpha to 0
+        and then given the best lambda we calibrate alpha.
+
+        :return: best_B, best_lambda, best_alpha
+        '''
+        _, best_lambda = self.calibrate_SPO_plus()
+        best_B = None
+        best_alpha = None
+        best_loss = np.PINF
+        for alpha_cur in self.alpha_sequence:
+            sp_reformulation_instance = SpReformulation(self.x_train, self.c_train, self.oracle, self.regularization,
+                                                        best_lambda, alpha_cur, self.time_limit)
+            sol_dict = sp_reformulation_instance.solve_model()
+            if sol_dict['status'] in ["infeasible", "inf_or_unbd"]:
+                pass
+            else:
+                B_new = sol_dict['B_ast']
+                if self.loss_metric == "SPO_loss":
+                    spo_loss_value = spo_loss(B_new, self.x_calibration, self.c_calibration, self.oracle)
+                    if spo_loss_value < best_loss:
+                        best_loss = spo_loss_value
+                        best_B = B_new
+                        best_alpha = alpha_cur
+
+        return best_B, best_lambda, best_alpha
--- tests/test_sp_calibrator.py
+++ tests/test_sp_calibrator.py
@@ -3,7 +3,7 @@ import logging
 import random
 import numpy as np
 
-from solver.sp_reformulation import SpReformulation
+from solver.sp_calibrator import SpCalibrator
 from utils.data_generator import SpDataGenerator
 from utils.sp_graph import SpGraph
 from oracle.sp_oracle import SpOracle
@@ -25,13 +25,13 @@ class MyUnitTest(unittest.TestCase):
         n_train = 100
         n_test = 10000
         holdout_percent = 0.25
-        n_holdout = np.floor(holdout_percent * n_train)
-        num_lambda = 10
-        lambda_max = 100
-        lambda_min_ratio = 10.0 ^ (-8)
+        n_holdout = int(holdout_percent * n_train)
+        num_lambda = num_alpha = 10
+        lambda_max = alpha_max = 100
+        lambda_min_ratio = alpha_min_ratio = 10.0 ** (-8)
 
         grid = SpGraph(dim)
-        sp_oracle = SpOracle(incidence_matrix=grid.A, start_node=0, end_node=24,
+        sp_oracle = SpOracle(incidence_matrix=grid.A, start_node=0, end_node=(grid.n_nodes - 1),
                              small_coefficient_tolerance=0.01,
                              time_limit=60)
 
@@ -46,6 +46,16 @@ class MyUnitTest(unittest.TestCase):
         x_test = data_generator.add_intercept(x_test)
         x_calibration = data_generator.add_intercept(x_calibration)
 
+        sp_calibrator_instance = SpCalibrator(x_train, c_train, x_calibration, c_calibration, sp_oracle, regularization,
+                                              time_limit, num_lambda,
+                                              lambda_max, lambda_min_ratio, num_alpha,
+                                              alpha_max, alpha_min_ratio)
+
+        # best_B_SPOplus, best_lambda_SPOplus, _ = sp_calibrator_instance.calibrate("SPO_plus")
+        best_B_LSE, best_lambda_LSE, _ = sp_calibrator_instance.calibrate("LSE")
+        # best_B_SPOplus_LSE, best_lambda_SPOplus_LSE, best_alpha_SPOplus_LSE = sp_calibrator_instance.calibrate("SPO_plus_LSE")
+
+        print(best_lambda_LSE)
 
 
 if __name__ == '__main__':
